9ca9177f1e4e69dc7a968bd7bd0718c1f2c79ed7
hoperyy <antfootmail@gmail.com>
2017-08-21 12:22:53 +0800

    bugfix: exclude

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const syncLocalFiles = require('./lib/local-syncfiles');
 const watchLocalFiles = require('./lib/local-watch');
 
-module.exports = (srcDir, targetDir, { type = 'hardlink', ignored = null, watch = false } = {}) => {
-    syncLocalFiles(srcDir, targetDir, { type, ignored });
+module.exports = (srcDir, targetDir, { type = 'hardlink', exclude = null, watch = false } = {}) => {
+    syncLocalFiles(srcDir, targetDir, { type, exclude });
 
     if (watch) {
-        const watcher = watchLocalFiles(srcDir, targetDir, { type, ignored });
+        const watcher = watchLocalFiles(srcDir, targetDir, { type, exclude });
         return watcher;
     }
 
