083d63e490d29c3530a32b4f7e3c93f913130df5
hoperyy <liuyuanyangscript@gmail.com>
2018-06-24 15:08:10 +0800

    add api: afterSync; api 'cb' not recommended

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const syncLocalFiles = require('./lib/local-syncfiles');
 const watchLocalFiles = require('./lib/local-watch');
 
-module.exports = (srcDir, targetDir, { type = 'hardlink', exclude = null, watch = false, cb = () => { } } = {}) => {
-    syncLocalFiles(srcDir, targetDir, { type, exclude });
+module.exports = (srcDir, targetDir, { type = 'hardlink', exclude = null, watch = false, cb = () => { }, afterSync = () => {} } = {}) => {
+    syncLocalFiles(srcDir, targetDir, { type, exclude, afterSync });
 
     if (watch) {
-        const watcher = watchLocalFiles(srcDir, targetDir, { type, exclude, cb });
+        const watcher = watchLocalFiles(srcDir, targetDir, { type, exclude, cb, afterSync });
         return watcher;
     }
 
