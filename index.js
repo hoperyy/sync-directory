eafd0c7e8dae7b2490619b7b63fefa844780a0e1
hoperyy <liuyuanyang@weidian.com>
2019-03-21 22:37:22 +0800

    feat: test hardlink support

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const syncLocalFiles = require('./lib/local-syncfiles');
 const watchLocalFiles = require('./lib/local-watch');
 
+const path = require('path');
+const fs = require('fs');
+const fse = require('fs-extra');
+
+const testHardlinkSupport = (targetDir) => {
+    const srcPath = path.join(targetDir, 'testHardlinkSupportSrcFilePath');
+    const targetPath = path.join(targetDir, 'testHardlinkSupportTargetFilePath');
+
+    let support = true;
+
+    // try {
+    //     fse.ensureFileSync(targetPath);
+    //     fse.ensureLinkSync(srcPath, targetPath);
+    // } catch(err) {
+    //     support = false;
+    // }
+
+    // if (fs.existsSync(srcPath)) {
+    //     fse.removeSync(srcPath);
+    // }
+    // if (fs.existsSync(targetPath)) {
+    //     fse.removeSync(targetPath);
+    // }
+
+    return support;
+};
+
 module.exports = (srcDir, targetDir, { type = 'hardlink', exclude = null, watch = false, deleteOrphaned = true, cb = () => { }, afterSync = () => {}, filter = () => true } = {}) => {
+    // test if support hardlink mode
+    fse.ensureDirSync(targetDir);
+
+    if (!testHardlinkSupport(targetDir)) {
+        type = 'copy';
+    }
+
     syncLocalFiles(srcDir, targetDir, { type, exclude, afterSync, deleteOrphaned, filter });
 
     if (watch) {
