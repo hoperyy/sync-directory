7f69f89a4b01478358be8e09776d5e2a0c892800
hoperyy <antfootmail@gmail.com>
2021-05-08 12:21:19 +0800

    feat: change d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
 export default function syncDirectory(srcDir: string, targetDir: string, options?: {
     type?: string;
-    forceSync?(filePath: string): boolean;
+    forceSync?(filePath?: string): boolean;
     exclude?: Array<string | RegExp | Function>;
     watch?: boolean;
     deleteOrphaned?: boolean;
     supportSymlink?: boolean;
-    afterSync?(filePath: string): any;
-    filter?(filePath: string): boolean;
-    onError?(err: object): any;
+    afterSync?(filePath?: string): any;
+    filter?(filePath?: string): boolean;
+    onError?(err?: object): any;
 }): object | void
