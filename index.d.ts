ae2ec1da99b4b3c0b6af7b53a485850f99029fa0
hoperyy <antfootmail@gmail.com>
2021-10-16 00:52:19 +0800

    feat: upgrade d.ts

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ export default function syncDirectory(srcDir: string, targetDir: string, options
     watch?: boolean;
     deleteOrphaned?: boolean;
     supportSymlink?: boolean;
-    afterSync?(params?: { type: string; relativePath: string }): any;
+    afterEachSync?(params?: { type: string; relativePath: string }): any;
     filter?(filePath?: string): boolean;
     onError?(err?: object): any;
     chokidarWatchOptions?: Object;
